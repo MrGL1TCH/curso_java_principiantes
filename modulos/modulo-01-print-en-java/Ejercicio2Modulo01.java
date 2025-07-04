5607f50466b15602bb65b49243fd0a9f4dff7fea
Angel <79184959+MrGL1TCH@users.noreply.github.com>
2025-07-03 18:13:14 -0600

    Corrección del formato de nombre de la carpeta modulo-01
    Cambio en el nombre de carpeta

---
+++ modulos/modulo-01-print-en-java/Ejercicio2Modulo01.java
@@ -0,0 +1,12 @@
+public class Ejercicio2Modulo01 {
+/*
+    Ejercicio 2 – Imprimir forma con asteriscos
+
+    Imprime una triángulo usando asteriscos (*) distribuidos en diferentes líneas.
+    Se practica el uso de `System.out.println` y cómo los espacios dentro de las comillas afectan la salida en consola.
+*/
+    public static void main(String[] args) {
+
+
+    }
+}

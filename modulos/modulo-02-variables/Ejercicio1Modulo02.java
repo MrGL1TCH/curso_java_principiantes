b5fc2c79a58dee3a8ce69c0e550bc479d4b80b52
Angel <79184959+MrGL1TCH@users.noreply.github.com>
2025-07-03 18:17:35 -0600

    Corrección de los archivos de la carpeta modulo-02
    Se añadieron los archivos correspondientes al modulo-02

---
+++ modulos/modulo-02-variables/Ejercicio1Modulo02.java
@@ -0,0 +1,13 @@
+public class Ejercicio1Modulo02 {
+/*
+    Crea una variable de tipo `String` llamada **saludo** que contenga alguna forma de saludar, puedes guiarte por el ejemplo.
+    Después imprimela.
+
+    Ejemplo de salida:
+    Hola
+ */
+    public static void main(String[] args) {
+
+
+    }
+}

dcb066c963f368a1600cd4f332f385e0d78b4a52
Angel <79184959+MrGL1TCH@users.noreply.github.com>
2025-07-07 20:15:44 -0600

    Delete modulos/modulo-01-print-en-java directory
    Eliminando para que los nombres de las carpetas sean iguales y tenga coherencia

--- modulos/modulo-01-print-en-java/HolaMundo.java
+++
@@ -1,5 +0,0 @@
-public class HolaMundo {   // Nombre de la clase igual al nombre del archivo
-    public static void main(String[] args) { 
-        System.out.println("¡Hola, mundo!"); // Imprimir el texto dentro de "" 
-    }
-}
